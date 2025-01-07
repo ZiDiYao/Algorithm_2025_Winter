e7983adb17317acdf34cc36354ded95dbea2f150
ZiDiYao <zidiyaocanada@outlook.com>
2025-01-07 21:16:45 +0800

    update recurssion tree

---
+++ src/Tree/IsBSTRecurssion.java
@@ -0,0 +1,12 @@
+package Tree;
+
+public class IsBSTRecurssion {
+    // 列可能性, assume 可以向左边树要信息, 可以向右边树要信息
+    // 1. 左边是 BST 右边是 BST
+    // 2. 左边的最大值应该小于当前 curr, 并且右边最小值应该大于 curr
+    // 所以, 我要向左边要什么信息? 第一, 最大值, 最小值, 和是否是搜索二叉树
+
+
+
+
+}
--- src/Tree/IsBalanced.java
+++ src/Tree/IsBalanced.java
@@ -1,12 +1,29 @@
 package Tree;
+import Binary_Search_Tree.Node;
 
 public class IsBalanced {
-
-    public class ReturnType{
-
-
+    public boolean isBalancedTree(Node node){
+        return process(node).isBalance;
 
     }
-
-
+    public class ReturnType{
+        public boolean isBalance;
+        public int height;
+        public ReturnType(boolean isB, int height){
+            this.isBalance = isB;
+            this.height = height;
+        }
+    }
+    public ReturnType process(Node node){
+        // base case
+        if (node == null){
+            return new ReturnType( true, 0);
+        }
+        ReturnType leftDate = process(node.left);
+        ReturnType rightDate = process(node.right);
+        int height = Math.max(leftDate.height, rightDate.height) + 1;
+        boolean isBalance = leftDate.isBalance && rightDate.isBalance
+                && Math.abs(leftDate.height - rightDate.height) < 2;
+        return new ReturnType(isBalance, height);
+    }
 }
